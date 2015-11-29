fa5fca977a46df5b540750be260b7339c0d6b9e9
José Aceituno <jalejandroaceituno@unitec.edu>
2015-11-29 17:10:18 -0600

    Subiendo método de Dijkstra

--- TonelitosMovil/src/tonelitosmovil/Grafo.java
+++ TonelitosMovil/src/tonelitosmovil/Grafo.java
@@ -102,7 +102,6 @@ public class Grafo {
     }
     
     public void removeEdge(Node vertex, Arista edge){
-        int nodeIndex=-1;
         
         for (int i = 0; i < nodos.size(); i++) {
             if (vertex.getID() == nodos.elementAt(i).getID()){
@@ -121,6 +120,47 @@ public class Grafo {
             }
         }
         
+    }
+    
+    public List Dijkstra(Node origin, Node destiny/*, List nodes*/){
+        List permanentes = new List();
+        List notPermanents = new List();
+        origin.setDijkstraNum(0);
+        permanentes.push_back(origin);
+        
+        for (int i = 0; i < nodos.size(); i++) {
+            nodos.elementAt(i).setDijkstraNum(Integer.MAX_VALUE);
+            nodos.elementAt(i).setDijkstraPath(new List());
+            if (nodos.elementAt(i) != origin) {
+                notPermanents.push_back(nodos.elementAt(i));
+            }
+        }
+        while(nodos.size() == permanentes.size()){
+            Node temp = permanentes.last();
+            for (int i = 0; i < temp.getAristas().size(); i++) {
+                for (int j = 0; j < permanentes.size(); j++) {
+                    if(((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal() != permanentes.elementAt(j)){
+                        if(((Arista)temp.getAristas().elementAt(i).getValue()).getDistancia()+temp.getDijkstraNum() < ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().getDijkstraNum()){
+                            ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().setDijkstraNum(((Arista)temp.getAristas().elementAt(i).getValue()).getDistancia()+temp.getDijkstraNum());
+                            ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().setDijkstraPath(temp.getDijkstraPath());
+                            ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().addToPath(((Arista)temp.getAristas().elementAt(i).getValue()));
+                        }
+                    }
+                }
+            }
+            long min = Integer.MAX_VALUE;
+            for (int i = 0; i < notPermanents.size(); i++) {
+                if (notPermanents.elementAt(i).getDijkstraNum() < min) {
+                    min = notPermanents.elementAt(i).getDijkstraNum();
+                    temp = notPermanents.elementAt(i);
+                }
+            }
+            int delete = notPermanents.find(temp);
+            notPermanents.remove(delete);
+            permanentes.push_back(temp);
+        }
+        
+        return origin.getDijkstraPath();
     }
     
     
