d31ee3eefa6d9d7beef098d7e07460284504479d
Mauricio Matamoros <mauricio.enrique.matamoros@gmail.com>
2015-11-29 20:37:32 -0600

    archivos

--- TonelitosMovil/src/tonelitosmovil/Grafo.java
+++ TonelitosMovil/src/tonelitosmovil/Grafo.java
@@ -10,7 +10,7 @@ package tonelitosmovil;
  * @author rick
  */
 public class Grafo {
-    
+
     private List nodos;
 
     public Grafo(List nodos) {
@@ -24,110 +24,110 @@ public class Grafo {
     public void setNodos(List nodos) {
         this.nodos = nodos;
     }
-    
-    public Node First(Node vertex){
-        int nodeIndex=-1;
-        
+
+    public Node First(Node vertex) {
+        int nodeIndex = -1;
+
         for (int i = 0; i < nodos.size(); i++) {
-            if (vertex.getID() == nodos.elementAt(i).getID()){
-                nodeIndex = ((Arista)nodos.elementAt(i).getAristas().elementAt(0).getValue()).getNodoFinal().getID();
+            if (vertex.getID() == nodos.elementAt(i).getID()) {
+                nodeIndex = ((Arista) nodos.elementAt(i).getAristas().elementAt(0).getValue()).getNodoFinal().getID();
                 break;
             }
         }
-        if (nodeIndex == -1){
+        if (nodeIndex == -1) {
             System.err.println("Index out of bounds");
             return null;
         }
         // el -1 se aplica si se empieza con ID 1 y no con ID 0
-        return nodos.elementAt(nodeIndex-1);
+        return nodos.elementAt(nodeIndex - 1);
     }
-    
-    public Node getVertex(Node vertex, int posicion){
-        int nodeIndex=-1;
-        
+
+    public Node getVertex(Node vertex, int posicion) {
+        int nodeIndex = -1;
+
         for (int i = 0; i < nodos.size(); i++) {
-            if (vertex.getID() == nodos.elementAt(i).getID()){
-                nodeIndex = ((Arista)nodos.elementAt(i).getAristas().elementAt(posicion).getValue()).getNodoFinal().getID();
+            if (vertex.getID() == nodos.elementAt(i).getID()) {
+                nodeIndex = ((Arista) nodos.elementAt(i).getAristas().elementAt(posicion).getValue()).getNodoFinal().getID();
                 break;
             }
         }
-        if (nodeIndex == -1){
+        if (nodeIndex == -1) {
             System.err.println("Index out of bounds");
             return null;
         }
         // el -1 se aplica si se empieza con ID 1 y no con ID 0
-        return nodos.elementAt(nodeIndex-1);
+        return nodos.elementAt(nodeIndex - 1);
     }
-    
-    public void addVertex(Node vertex){
-        nodos.elementAt(nodos.size()-1).setNext(vertex);
+
+    public void addVertex(Node vertex) {
+        nodos.elementAt(nodos.size() - 1).setNext(vertex);
     }
-    
-    public void addEdge(Node vertex, Arista edge){
-        
+
+    public void addEdge(Node vertex, Arista edge) {
+
         for (int i = 0; i < nodos.size(); i++) {
-            if (vertex.getID() == nodos.elementAt(i).getID()){
+            if (vertex.getID() == nodos.elementAt(i).getID()) {
                 nodos.elementAt(i).getAristas().push_back(edge);
                 break;
             }
         }
     }
-    
-    public void removeVertex(int posicion){
-        int nodeIndex=-1;
-        
+
+    public void removeVertex(int posicion) {
+        int nodeIndex = -1;
+
         for (int i = 0; i < nodos.size(); i++) {
-            if (nodos.elementAt(posicion).getID() == nodos.elementAt(i).getID()){
+            if (nodos.elementAt(posicion).getID() == nodos.elementAt(i).getID()) {
                 nodeIndex = i;
                 break;
             }
         }
-        
-        if (nodeIndex != -1){
+
+        if (nodeIndex != -1) {
             for (int i = 0; i < nodos.size(); i++) {
                 for (int j = 0; j < nodos.elementAt(i).getAristas().size(); j++) {
-                    if (((Arista)nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoFinal().getID() ==
-                            nodos.elementAt(nodeIndex).getID() 
-                            || ((Arista)nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoInicial().getID() ==
-                            nodos.elementAt(nodeIndex).getID() ){
-                        removeEdge(nodos.elementAt(nodeIndex), ((Arista)nodos.elementAt(i).getAristas().elementAt(j).getValue()));
+                    if (((Arista) nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoFinal().getID()
+                            == nodos.elementAt(nodeIndex).getID()
+                            || ((Arista) nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoInicial().getID()
+                            == nodos.elementAt(nodeIndex).getID()) {
+                        removeEdge(nodos.elementAt(nodeIndex), ((Arista) nodos.elementAt(i).getAristas().elementAt(j).getValue()));
                     }
                 }
             }
-            
+
             nodos.remove(nodeIndex);
-        }else{
+        } else {
             System.err.println("Index out of bounds");
         }
     }
-    
-    public void removeEdge(Node vertex, Arista edge){
-        
+
+    public void removeEdge(Node vertex, Arista edge) {
+
         for (int i = 0; i < nodos.size(); i++) {
-            if (vertex.getID() == nodos.elementAt(i).getID()){
+            if (vertex.getID() == nodos.elementAt(i).getID()) {
                 for (int j = 0; j < nodos.elementAt(i).getAristas().size(); j++) {
-                    if (((Arista)nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoInicial().getID() ==
-                            edge.getNodoInicial().getID() 
-                            && ((Arista)nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoFinal().getID() ==
-                            edge.getNodoFinal().getID()){
-                        
-                       nodos.elementAt(i).getAristas().remove(j);
-                       break;
-                        
-                    } 
+                    if (((Arista) nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoInicial().getID()
+                            == edge.getNodoInicial().getID()
+                            && ((Arista) nodos.elementAt(i).getAristas().elementAt(j).getValue()).getNodoFinal().getID()
+                            == edge.getNodoFinal().getID()) {
+
+                        nodos.elementAt(i).getAristas().remove(j);
+                        break;
+
+                    }
                 }
-                
+
             }
         }
-        
+
     }
-    
-    public List Dijkstra(Node origin, Node destiny/*, List nodes*/){
+
+    public List Dijkstra(Node origin, Node destiny/*, List nodes*/) {
         List permanentes = new List();
         List notPermanents = new List();
         origin.setDijkstraNum(0);
         permanentes.push_back(origin);
-        
+
         for (int i = 0; i < nodos.size(); i++) {
             nodos.elementAt(i).setDijkstraNum(Integer.MAX_VALUE);
             nodos.elementAt(i).setDijkstraPath(new List());
@@ -135,15 +135,15 @@ public class Grafo {
                 notPermanents.push_back(nodos.elementAt(i));
             }
         }
-        while(nodos.size() == permanentes.size()){
+        while (nodos.size() == permanentes.size()) {
             Node temp = permanentes.last();
             for (int i = 0; i < temp.getAristas().size(); i++) {
                 for (int j = 0; j < permanentes.size(); j++) {
-                    if(((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal() != permanentes.elementAt(j)){
-                        if(((Arista)temp.getAristas().elementAt(i).getValue()).getDistancia()+temp.getDijkstraNum() < ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().getDijkstraNum()){
-                            ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().setDijkstraNum(((Arista)temp.getAristas().elementAt(i).getValue()).getDistancia()+temp.getDijkstraNum());
-                            ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().setDijkstraPath(temp.getDijkstraPath());
-                            ((Arista)temp.getAristas().elementAt(i).getValue()).getNodoFinal().addToPath(((Arista)temp.getAristas().elementAt(i).getValue()));
+                    if (((Arista) temp.getAristas().elementAt(i).getValue()).getNodoFinal() != permanentes.elementAt(j)) {
+                        if (((Arista) temp.getAristas().elementAt(i).getValue()).getDistancia() + temp.getDijkstraNum() < ((Arista) temp.getAristas().elementAt(i).getValue()).getNodoFinal().getDijkstraNum()) {
+                            ((Arista) temp.getAristas().elementAt(i).getValue()).getNodoFinal().setDijkstraNum(((Arista) temp.getAristas().elementAt(i).getValue()).getDistancia() + temp.getDijkstraNum());
+                            ((Arista) temp.getAristas().elementAt(i).getValue()).getNodoFinal().setDijkstraPath(temp.getDijkstraPath());
+                            ((Arista) temp.getAristas().elementAt(i).getValue()).getNodoFinal().addToPath(((Arista) temp.getAristas().elementAt(i).getValue()));
                         }
                     }
                 }
@@ -159,10 +159,11 @@ public class Grafo {
             notPermanents.remove(delete);
             permanentes.push_back(temp);
         }
-        
+
         return origin.getDijkstraPath();
     }
-       public List FLoyd() {
+
+    public List FLoyd() {
         List optimos = new List();
         List lista;
         List ponderaciones = new List();
@@ -185,6 +186,7 @@ public class Grafo {
         }
 
         lista = Camino(caminos, ponderaciones);
+        ponderaciones = llenar(ponderaciones);
 
         //lleno caminos
         //lleno 
@@ -219,19 +221,17 @@ public class Grafo {
             for (int j = 0; j < ponderaciones.size(); j++) {
                 ((List) ponderaciones.elementAt(i).getValue()).elementAt(i).setValue(0);
                 for (int k = 0; k < ponderaciones.size(); k++) {
-                    if ((int)((List) ponderaciones.elementAt(i).getValue()).elementAt(j).getValue() != 0
-                        && ((Arista)nodos.elementAt(j).getAristas().elementAt(k).getValue()).getNodoFinal().getID() == j ) {
-                        ((List) ponderaciones.elementAt(i).getValue()).elementAt(i).setValue(((Arista)nodos.elementAt(j).getAristas().elementAt(k).getValue()).getDistancia());
-                } else {
-                    ((List) ponderaciones.elementAt(i).getValue()).elementAt(i).setValue(Integer.MAX_VALUE);
+                    if ((int) ((List) ponderaciones.elementAt(i).getValue()).elementAt(j).getValue() != 0
+                            && ((Arista) nodos.elementAt(j).getAristas().elementAt(k).getValue()).getNodoFinal().getID() == j) {
+                        ((List) ponderaciones.elementAt(i).getValue()).elementAt(i).setValue(((Arista) nodos.elementAt(j).getAristas().elementAt(k).getValue()).getDistancia());
+                    } else {
+                        ((List) ponderaciones.elementAt(i).getValue()).elementAt(i).setValue(Integer.MAX_VALUE);
+                    }
                 }
-                }
-                
+
             }
         }
         return ponderaciones;
     }
 
-
-    
 }
